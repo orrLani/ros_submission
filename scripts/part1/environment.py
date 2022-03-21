1f1a59c7bbd3d8b8a7d008be78548533adeabd8c
Invisul <awesomeitay@gmail.com>
2022-03-21 23:35:48 +0300

    final

--- scripts/part1/environment.py
+++ scripts/part1/environment.py
@@ -14,6 +14,10 @@ import subprocess
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from mapPointDirty import map_point_dirty
 
+from nav_msgs.srv import GetPlan
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
+import actionlib
+from move_base_msgs.msg import MoveBaseAction
 def Diff(li1, li2):
     return list(set(li1) - set(li2))
 
@@ -26,9 +30,13 @@ class Env():
         self.action_size = action_size
         self.initGoal = True
         self.get_goalbox = False
-        self.position = Pose()
+        self.position = Pose().position
+        self.rival_position = Pose().position
+        self.myscore = 0
+        self.rivalscore = 0
+        self.reason = None
         self.pub_cmd_vel = rospy.Publisher('/tb3_{}/cmd_vel'.format(agent_id), Twist, queue_size=5)
-
+        self.client = actionlib.SimpleActionClient('tb3_0/move_base', MoveBaseAction)
         # gazebo messages
         self.reset_proxy = rospy.ServiceProxy('gazebo/reset_simulation', Empty)
         # self.unpause_proxy = rospy.ServiceProxy('gazebo/unpause_physics', Empty)
@@ -69,6 +77,10 @@ class Env():
             # no update
             return
         ## yyyy
+        flag = False
+        if (len(str(self.dirty_location_msg))) > 0:
+            flag = True
+
         rospy.loginfo("update dirty!")
         self.dirty_location_msg = msg.data
         rospy.loginfo(msg.data)
@@ -83,12 +95,25 @@ class Env():
         _dirty_list = _dirty_string.split(',')
 
         dirty_list = []
+
         for i in range(0,len(_dirty_list[:-1]),2):
             dirty_list.append((float(_dirty_list[i]),float(_dirty_list[i+1])))
 
         # check if we need to create dirty or delete dirty
         dirty_collect = Diff(self.dirty_location, dirty_list)
         self.dirty_location = dirty_list
+
+        ## how to know who collected
+
+        if flag:
+            x, y = self.position.x, self.position.y
+            x_rival, y_rival = self.rival_position.x, self.rival_position.y
+
+            if dist([x,y],dirty_collect[0]) > dist([x_rival,y_rival],dirty_collect[0]):
+                self.rivalscore = self.rivalscore + 1
+            else:
+                self.myscore = self.myscore + 1
+        ##
 
         if len(dirty_collect) > 0:
             # need to delete dirty
@@ -105,6 +130,7 @@ class Env():
             for dirty in self.map_point_dirty_list:
                 dirty.create_square()
 
+
     def getMinDistace(self):
         temp_list = []
         for i, map_point_dirty in enumerate(self.map_point_dirty_list):
@@ -114,7 +140,7 @@ class Env():
         return min(temp_list)
 
     def get_opp_odometry(self, odom):
-        pass
+        self.rival_position = odom.pose.pose.position
 
     def get_my_odometry(self, odom):
         self.position = odom.pose.pose.position
@@ -131,6 +157,8 @@ class Env():
 
         elif heading < -pi:
             heading += 2 * pi
+
+
 
         self.heading = round(heading, 2)
 
@@ -154,24 +182,106 @@ class Env():
         current_distance = round(math.hypot(self.goal_x - self.position.x, self.goal_y - self.position.y),2)
         if current_distance < 0.2:
             self.get_goalbox = True
-
+        done_flag = 0
         # conditions for done
         # by min range
         if min_range > min(scan_range) > 0:
+            done_flag = -1
             done = True
 
         # robot finish collecting all the dirty
         if len(self.map_point_dirty_list) == 0:
             done = True
 
+        if self.myscore > len(self.map_point_dirty_list)+self.rivalscore:
+            done_flag=1
+            done = True
+        if self.rivalscore > len(self.map_point_dirty_list)+self.myscore:
+            done_flag= -2
+            done = True
         # TODO ADD CONDITION - if itai Mechonot get more points from us.
 
-        return scan_range + [heading, current_distance], done
+        score_diff = self.myscore - self.rivalscore
+        my_location = [self.position.x , self.position.y]
+        rival_location = [self.rival_position.x, self.rival_position.y]
+        dirt_locations = self.dirty_location
+        output =  [score_diff] + my_location + rival_location + scan_range + [heading]
+        return  output, done, done_flag , dirt_locations
+        # return scan_range + [heading, current_distance], done
 
 
-    def setReward(self, state, done, action):
-        # the reward function, here we need to change the function.
-        return 1
+    def setReward(self, state, done, action, done_flag , dirt_list):
+        yaw_reward = []
+        current_distance = state[-1]
+        heading = state[-1]
+
+        # for i in range(5):
+        #     angle = -pi / 4 + heading + (pi / 8 * i) + pi / 2
+        #     tr = 1 - 4 * math.fabs(0.5 - math.modf(0.25 + 0.5 * angle % (2 * math.pi) / math.pi)[0])
+        #     yaw_reward.append(tr)
+        #
+        # distance_rate = 2 ** (current_distance / self.goal_distance)
+        # reward = ((round(yaw_reward[action] * 5, 2)) * distance_rate)
+        ###
+
+
+        start = PoseStamped()
+        start.header.seq = 0
+        start.header.frame_id = "map"
+        start.header.stamp = rospy.Time(0)
+        start.pose.position.x = float(state[1])
+        start.pose.position.y = float(state[2])
+        array = quaternion_from_euler(0,0, heading)
+        start.pose.orientation.w = float(array[2])
+        start.pose.orientation.z = float(array[3])
+
+        Goal = PoseStamped()
+        Goal.header.seq = 0
+        Goal.header.frame_id = "map"
+        Goal.header.stamp = rospy.Time(0)
+
+        _ = actionlib.SimpleActionClient('/tb3_0/move_base', MoveBaseAction)
+        get_plan = rospy.ServiceProxy('/tb3_0/move_base/make_plan', GetPlan)
+        req = GetPlan()
+        req.start = start
+
+        total_dist_sum = 0
+        for point in dirt_list:
+            Goal.pose.position.x = float(point[0])
+            Goal.pose.position.y = float(point[1])
+            req.goal = Goal
+            req.tolerance = .5
+            resp = get_plan(req.start, req.goal, req.tolerance)
+            total_dist_sum = total_dist_sum + len(resp.plan.poses)
+
+        dirt_distane_reward = 1/(1+total_dist_sum)
+
+        score_diff_reward = state[0]/(1+len(dirt_list))
+
+
+
+        ###
+        reward = score_diff_reward + dirt_distane_reward
+
+        if done:
+            reward = 0
+            # collision!
+            if done_flag == -1:
+                rospy.loginfo("Collision!!")
+                reward = -200
+            # incase we won (BIG REWARD) and cant lose (not enough dirt on stage to lose)
+            if done_flag == 1:
+                rospy.loginfo("Won!!")
+                reward = 200
+            # incase we cant win anymore ( rival collected more dirt)
+            if done_flag == -2:
+                rospy.loginfo("Lost!!")
+                reward = -200
+            self.pub_cmd_vel.publish(Twist())
+
+
+
+        return reward
 
     def step(self, action):
         # doing a step in the environment.
@@ -182,7 +292,7 @@ class Env():
         # doing a step in the env (in this case change the velocity)
         ang_vel = ((self.action_size - 1)/2 - action) * max_angular_vel * 0.5
         vel_cmd = Twist()
-        vel_cmd.linear.x = 0.15
+        vel_cmd.linear.x = 0.25
         vel_cmd.angular.z = ang_vel
         # pub the velocity ( in our case we need to think about some think else)
         self.pub_cmd_vel.publish(vel_cmd)
@@ -196,13 +306,13 @@ class Env():
                 pass
 
         # change the state by the data scan.
-        state, done = self.getState(data)
+        state, done , done_flag,  dirt_list = self.getState(data)
 
         # set the reward
-        reward = self.setReward(state, done, action)
+        reward = self.setReward(state, done, action, done_flag, dirt_list)
 
-        # TODO REMOVE DONE = FALSE
-        done = False
+        # # TODO REMOVE DONE = FALSE
+        # done = False
         # check if is done by env
         return np.asarray(state), reward, done
 
@@ -251,10 +361,14 @@ class Env():
         # rviz process
         if self.rviz_proc is not None:
             self.rviz_proc.kill()
+        time.sleep(10)
+        self.rviz_proc = subprocess.Popen('roslaunch MRS_236609 multi_bringup.launch', shell=True)
+        time.sleep(10)
+        rospy.loginfo("Rviz was reset ")
+
         self.dirty_proc = subprocess.Popen('rosrun MRS_236609 dirt_publisher_ex3.py', shell=True)
         self.dummy_agent_proc = subprocess.Popen('rosrun ros_submission dummy_agent.py', shell=True)
-        self.rviz_proc = subprocess.Popen('roslaunch MRS_236609 multi_bringup.launch', shell=True)
-        time.sleep(5)
+
         # subprocess.call('/home/orr/my_ws/src/ros_submission/part1/scripts/ros_rviz_shell')
         # subprocess.call('/home/orr/my_ws/src/ros_submission/scripts/ros_dirty_shell')
         # time.sleep(10)
@@ -281,7 +395,13 @@ class Env():
         # TODO create dirty function
         # for dirty in self.map_point_dirty_list:
         #    _,_ = dirty.create_square()
-
-        state, _ = self.getState(data)
+        self.myscore = 0
+        self.rivalscore = 0
+        self.reason = None
+        state, _, _, _ = self.getState(data)
 
         return np.asarray(state)
+
+
+def dist(l1, l2):
+    return pow((pow((l1[0] - l2[0]), 2) + pow((l1[1] - l2[1]), 2)), 0.5)
